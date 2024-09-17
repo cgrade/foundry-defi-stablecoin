73f8cb04e289c1cdb6de0bc222485458439edea4
Abraham Elijah <gradeisageek@gmail.com>
2024-09-17 15:41:18 +0100

    Deploy Script: Script to deploy the 'Decentralized Stable Coin(DSC) and the DSC Engine' contracts

---
+++ script/DeployDSC.s.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {Script} from "forge-std/Script.sol";
+import {DecentralizedStableCoin} from "../src/DecntralizedStableCoin.sol";
+import {DSCEngine} from "../src/DSCEngine.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+contract DeployDSC is Script {
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
+    function run() public returns (DecentralizedStableCoin, DSCEngine, HelperConfig) {
+
+        HelperConfig config = new HelperConfig();
+
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) = config.activeNetworkConfig();
+
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+        tokenAddresses = [weth, wbtc];
+
+        // Deploy DSC
+        vm.startBroadcast(deployerKey);
+        DecentralizedStableCoin dsc = new DecentralizedStableCoin();
+        DSCEngine engine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
+        dsc.transferOwnership(address(engine));
+        vm.stopBroadcast();
+        return(dsc, engine, config);
+    }
+}
